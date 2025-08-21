2a52dfc64ee69af08a2ff91c9a3f2fc82c925264
wucheng <1965417564@qq.com>
2025-08-21 10:40:30 +0800

    :recycle: refactor(index.ts): 优化导出结构，直接导出interface和types模块

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ import * as anyDecorator from './decorator/index'
 import * as anyEntity from './entity/index'
 import * as anyEnum from './enum/index'
 import * as anyHelper from './helper/index'
-import * as anyInterface from './interface/index'
 import * as anyModel from './model/index'
-import * as anyTypes from './types/index'
+
+export * from './interface/index'
+export * from './types/index'
 
 export default {
   ...anyDecorator,
   ...anyEntity,
   ...anyEnum,
   ...anyHelper,
-  ...anyInterface,
   ...anyModel,
-  ...anyTypes,
 }
