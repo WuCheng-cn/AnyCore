d3b30256601b5c3400a04e788295963c54748718
wucheng <1965417564@qq.com>
2025-08-28 11:43:30 +0800

    :recycle: refactor: 将继承改为Partial类型以增加灵活性
    修改ISearchFieldConfig接口继承自Partial<IFormFieldConfig>，使得所有表单字段配置属性变为可选，提高接口使用的灵活性

--- src/interface/ISearchFieldConfig.ts
+++ src/interface/ISearchFieldConfig.ts
@@ -1,6 +1,6 @@
 import type { IFormFieldConfig } from './IFormFieldConfig'
 
-export interface ISearchFieldConfig extends IFormFieldConfig {
+export interface ISearchFieldConfig extends Partial<IFormFieldConfig> {
   /** # 是否为高级搜索字段（配置后会在高级搜索中显示） */
   isAdvanced?: boolean
 }
