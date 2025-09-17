208d73ab96fb386e7bbcf8346417be95e126fe86
wucheng <1965417564@qq.com>
2025-09-17 15:42:35 +0800

    :recycle: refactor(interface): 重命名弹窗事件属性并调整类型
    将 onConfirm 改为 confirm 以保持命名一致性
    添加 close 方法作为必选属性，将 onClosed 改为可选属性

--- src/interface/IDialogProps.ts
+++ src/interface/IDialogProps.ts
@@ -7,13 +7,16 @@ interface IDialogProps<T> {
   title?: string
 
   /** # 弹窗的确认按钮被点击事件（通过该事件向外抛出数据） */
-  onConfirm: (param: T) => void
+  confirm: (param: T) => void
+
+  /** # 弹窗的关闭事件（通过该事件关闭弹窗） */
+  close: () => void
 
   /** # 关闭前的钩子 */
   beforeClose?: () => Promise<void>
 
-  /**  # 弹窗的关闭事件（通过该事件关闭弹窗） */
-  onClosed: () => void
+  /** # 弹窗关闭后的钩子 */
+  onClosed?: () => void
 
   /** # 最小化前的钩子 */
   beforeMinimize?: () => Promise<void>
